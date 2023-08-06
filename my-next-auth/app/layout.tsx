70ce06ed2719244745ea7e38b6b60d438ed6a4e4
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-06 13:08:45 -0700

    Created AuthProvider to wrap rootNode

--- my-next-auth/app/layout.tsx
+++ my-next-auth/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from "@/components/Navbar";
+import AuthProvider from "@/components/AuthProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-      <NavBar/>{children}</body>
-    </html>
+      <AuthProvider>
+        <html lang="en">
+          <body className={inter.className}>
+          <NavBar/>{children}</body>
+        </html>
+      </AuthProvider>
   )
 }
