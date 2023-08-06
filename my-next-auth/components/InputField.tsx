a58a5a9295dc7838cafc1a6e5d8f476d39a964df
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-06 13:08:20 -0700

    Fast fix: text white override to black

--- my-next-auth/components/InputField.tsx
+++ my-next-auth/components/InputField.tsx
@@ -20,6 +20,7 @@ const InputField: React.FC<InputFieldProps> = ({ label, name, value, onChange })
                 value={value}
                 onChange={onChange}
                 className="mt-1 px-3 py-2 w-full border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+                style={{ color: "black" }}
             />
         </div>
     );
